7ab474c964caf702f961e8d1e7f95ac1bbe60c99
IldarSaygafarov2 <sayildar17@gmail.com>
2024-06-20 15:36:59 +0500

    added fixes

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserCustomAdmin(UserAdmin):
+	list_display_links = ['first_name', 'email']
 	fieldsets = (
 		(None, {"fields": ("username", "password")}),
 		(_("Personal info"), {"fields": ("first_name", "last_name", "surname", "email")}),
--- users/models.py
+++ users/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
 
 class User(AbstractUser):
+	username = models.CharField(verbose_name='Имя пользователя', max_length=100, unique=False, null=True, blank=True)
 	surname = models.CharField(verbose_name='Отчество', max_length=100)
 	phone_number = models.CharField(verbose_name='Номер телефона', max_length=20, null=True, blank=True)
 	birth_date = models.CharField(verbose_name='Дата рождения', max_length=20, null=True, blank=True)
 	email = models.EmailField(unique=True)
 	
-	REQUIRED_FIELDS = ['phone_number']	
+	objects = CustomUserManager()
+
+	REQUIRED_FIELDS = []	
 	USERNAME_FIELD = 'email'
 	
-	class Meta:
-		unique_together = ['email']
